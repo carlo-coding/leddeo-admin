384eca9eed030050386407be7bdf0fdf5534e2a9
carlo-coding <jaanc.rt@gmail.com>
2023-02-11 22:32:22 -0600

    added same-origin credentials

--- src/utils/isAdmin.ts
+++ src/utils/isAdmin.ts
@@ -36,7 +36,7 @@ export async function isAdmin({
   const resp = await fetch(`${apiUrl}/admin/login/?next=/admin/`, {
     method: "POST",
     body: formData,
-    credentials: "same-origin",
+    credentials: "include",
   });
   const data = await resp.text();
   const parser = new DOMParser();
